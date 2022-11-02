7087103b7d5a7c8f47ddddf350cf935b7b3b1bb3
Compi-Craft <ozarko.pn@ucu.edu.ua>
2022-11-02 10:34:12 +0200

    created function 'get words'

--- target_game.py
+++ target_game.py
@@ -22,12 +22,27 @@ def generate_grid() -> List[List[str]]:
         game_lst.append(small_lst)
         i += 1
     return game_lst
-def get_words(f: str, letters: List[str]) -> List[str]:
+def get_words(file_read: str, letters: List[str]) -> List[str]:
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
-    pass
-
+    with open(file_read, "r", encoding="utf-8") as file:
+        word_lst = []
+        for i in file:
+            i = i.replace("\n", "")
+            word_lst.append(i.lower())
+    fin_lst = []
+    for i in word_lst:
+        if len(i) >= 4 and letters[4] in i:
+            for k in i:
+                if k in letters and i.count(k) <= letters.count(k):
+                    check = True
+                else:
+                    check = False
+                    break
+            if check is True:
+                fin_lst.append(i)
+    return fin_lst
 
 
 def get_user_words() -> List[str]:
